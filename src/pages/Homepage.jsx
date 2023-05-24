78697d56ac44251a9f425a4fa343aced0d4b050b
Nsikak Akpan <builtbykaksy@gmail.com>
2023-05-24 04:30:02 +0100

    5th Update
    built the Loading component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Loading from '../components/loading/Loading';
 
 const Homepage = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const delay = 2000; // 2 seconds delay
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+  }, []);
+
   return (
-    <div>Homepage</div>
+    <>
+    {
+        isLoading ? (
+            <Loading/>
+        ) : (
+            <div className='homepage'>
+                <div className="overlay"></div>
+                <div>HomePage</div>
+            </div>
+        )
+    }
+    </>
+   
   )
 }
 
