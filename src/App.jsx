21ec68585c604295192d0cd3b05e10df6e2bd4f9
Nsikak Akpan <builtbykaksy@gmail.com>
2023-05-24 04:11:14 +0100

    2nd update
    installation of all dependencies and setup of project structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState, useRef } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Homepage from './pages/Homepage';
+import Portfoliopage from './pages/Portfoliopage';
 
 function App() {
-  const [count, setCount] = useState(0)
+ //message popup
+ const [visible, setVisible] = useState(false);
+ const [isDark, setIsDark] = useState(false);
+ const cardRef = useRef(null);
+
+ useEffect(() => {
+   let handleOutsideClick = (e) => {
+     if(!cardRef.current.contains(e.target)){        
+       setIsOpen(false);
+     }      
+   }
+
+   document.addEventListener('mousedown', handleOutsideClick)
+   
+   return () => {
+     document.removeEventListener('mousedown', handleOutsideClick);
+   };
+ }, []);
+
+//theme-modal
+const [isOpen, setIsOpen] = useState(false);
+
+function openThemeModal(){
+setIsOpen(true)
+console.log('clicked theme modal')
+}
+
+useEffect(() => {
+const currentThemeColor = localStorage.getItem("theme-color")
+if(currentThemeColor === "dark"){
+ setIsDark(true)
+}else{
+ setIsDark(false)
+}
+},[])
+
+const toggleTheme = () => {
+if(isDark){
+ localStorage.setItem("theme-color", "light")
+ setIsDark(false)
+}else{
+ localStorage.setItem("theme-color", "dark")
+ setIsDark(true)
+}
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route index element={<Homepage setVisible={setVisible} openThemeModal={openThemeModal}/>}/>
+        <Route path='/projects' element={<Portfoliopage openThemeModal={openThemeModal}/>}/>
+      </Routes> 
     </>
   )
 }
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+import { FiExternalLink } from 'react-icons/fi'
+
+const ProjectCard = ({title, technologies, imgUrl, github, site}) => {
+  return (
+    <div className="portfolio__item">                            
+        <div className="image">
+            <img src={imgUrl} alt=""/>
+        </div>
+        <h3>{title}</h3>
+        <p className="blog-text">{technologies}</p>
+        <div className="hover-items">
+            <div className="wrap">
+                <h3>Project Source</h3>
+                <div className="icons">
+                    <a href={github} target='_blank' className="icon">
+                        <BsGithub/>
+                        <p>Code</p>
+                    </a>
+                    <a href={site} target='_blank' className="icon">
+                        <FiExternalLink/>
+                        <p>Demo</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard;
