0de206b20061cf4f0eb2b215abd506400ba63f7e
Nsikak Akpan <builtbykaksy@gmail.com>
2023-05-24 08:13:18 +0100

    new update
    error fixed

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -11,6 +11,7 @@ import img9 from '../assets/school.png'
 import './portfoliopage.css'
 import NavBar from '../components/navbar/Navbar'
 import ProjectCard from '../components/ProjectCard'
+import Footer from '../components/footer/Footer'
 
 const ProjectPage = ({openThemeModal}) => {
   return (
@@ -22,9 +23,72 @@ const ProjectPage = ({openThemeModal}) => {
                 Here are some of my work that I've done in various technologies.
             </p>
             <div className="container portfolio__container">
-            
+            <ProjectCard 
+                title="Netflix Clone" 
+                technologies="Built using React Js, CSS, Firebase and Redux." 
+                imgUrl={img1}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />
+            <ProjectCard 
+                title="Amazon Clone" 
+                technologies="Built using Next JS, Tailwind CSS and Firebase" 
+                imgUrl={img2}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />           
+            <ProjectCard 
+                title="Youtube Clone" 
+                technologies="Built using React JS, CSS, Firebase and Redux." 
+                imgUrl={img4}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />
+            <ProjectCard 
+                title="Responsive Landing Page" 
+                technologies="Built using HTML, CSS and JavaScript" 
+                imgUrl={img5}
+                github="https://github.com/Kaksie-codes/GPT-3-Kaksy"
+                site='https://gpt-kaksy.netlify.app/'
+            />
+            <ProjectCard 
+                title="Geritch Restaurant Website" 
+                technologies="Built using React JS and CSS" 
+                imgUrl={img6}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />
+            <ProjectCard 
+                title="Fitness Website" 
+                technologies="Built using React JS and CSS" 
+                imgUrl={img7}
+                github="https://github.com/Kaksie-codes/MyFitnessApp"
+                site='https://myfitnessapp007.netlify.app/'
+            />
+            <ProjectCard 
+                title="Multipage School Website" 
+                technologies="Built using HTML, CSS and JavaScript" 
+                imgUrl={img9}
+                github="https://github.com/Kaksie-codes/Multipage-School-Website"
+                site='https://jessyschools.netlify.app/'
+            />
+            <ProjectCard 
+                title="Spotify Clone" 
+                technologies="Built using Next JS, Tailwind CSS and Firebase" 
+                imgUrl={img8}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />            
+             <ProjectCard 
+                title="Blink - Social media App" 
+                technologies="Built using Next JS, Tailwind CSS and Firebase" 
+                imgUrl={img3}
+                github=""
+                site='https://jessyschools.netlify.app/'
+            />
             </div>
         </div>
+        <Footer/>
     </div>
   )
 }
